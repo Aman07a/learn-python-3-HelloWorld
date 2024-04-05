706298094b1e65c7b915a3e6af2f3d3bac0c3f31
Aman07a <aman16.aa17@gmail.com>
2024-04-05 11:41:49 +0200

    Added python-block-letters

---
+++ python-block-letters.py
@@ -0,0 +1,9 @@
+# First and last name: John Doe
+# Fun fact about myself: I love hiking in the mountains.
+
+print("J" * 5 + "  " + "D" + " " * (3) + "D")
+print("    J" + " " * 4 + "D" + " " * (3) + "D")
+print("    J" + " " * 4 + "D" + " " * (3) + "D")
+print("J   J" + " " * 3 + "D" + " " * (3) + "D")
+print("J   J" + " " * 3 + "D" + " " * (3) + "D")
+print(" JJJ " + " " * 3 + "D" + " " * (3) + "D")
