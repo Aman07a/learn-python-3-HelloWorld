90acaf437cc1afc25d4c6d5f969afd43a738a755
Aman07a <aman16.aa17@gmail.com>
2024-04-05 11:00:48 +0200

    Added review

---
+++ review.py
@@ -0,0 +1,13 @@
+# Create variables
+my_age = 23
+half_my_age = my_age / 2
+greeting = "Hello"
+name = "Aman"
+greeting_with_name = greeting + ", " + name
+
+# Print variables
+print("My age:", my_age)
+print("Half my age:", half_my_age)
+print("Greeting:", greeting)
+print("Name:", name)
+print("Greeting with name:", greeting_with_name)
