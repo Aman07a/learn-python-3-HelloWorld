691b682a9aa9e3c9bf2f513061d6d26005f3ad8d
Aman07a <aman16.aa17@gmail.com>
2024-04-05 10:56:09 +0200

    Added multiline-strings

---
+++ multi-line-strings.py
@@ -0,0 +1,7 @@
+# Assign the multi-line string to the variable 'to_you'
+to_you = """
+Stranger, if you passing meet me and desire to speak to me, why should you not speak to me?
+And why should I not speak to you?
+"""
+
+print(to_you)
