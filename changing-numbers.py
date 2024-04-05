39b31afc12c9f4eea68759627e70a34ce3749f7c
Aman07a <aman16.aa17@gmail.com>
2024-04-05 10:25:07 +0200

    Added changing-numbers

---
+++ changing-numbers.py
@@ -0,0 +1,14 @@
+# Define variables for quilt width and length
+quilt_width = 8
+quilt_length = 12
+
+# Calculate the number of squares needed for the original quilt
+total_squares_original = quilt_width * quilt_length
+print("Number of squares needed for the original quilt:", total_squares_original)
+
+# Update quilt_length to make the quilt shorter
+quilt_length = 8
+
+# Calculate the number of squares needed for the modified quilt
+total_squares_modified = quilt_width * quilt_length
+print("Number of squares needed for the modified quilt:", total_squares_modified)
