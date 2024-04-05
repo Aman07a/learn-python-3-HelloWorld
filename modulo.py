ea30b2fdb825c57f4c526a6d244a3a12cd2cbd28
Aman07a <aman16.aa17@gmail.com>
2024-04-05 10:38:34 +0200

    Added modulo

---
+++ modulo.py
@@ -0,0 +1,25 @@
+# Calculate the remainder for order #263
+order_263_r = 263 % 11
+print(order_263_r)
+
+# Determine if order #263 should receive a coupon
+if order_263_r == 0:
+    order_263_coupon = "yes"
+else:
+    order_263_coupon = "no"
+
+# Print out whether order #263 should receive a coupon
+print(order_263_coupon)
+
+# Calculate the remainder for order #264
+order_264_r = 264 % 11
+print(order_264_r)
+
+# Determine if order #264 should receive a coupon
+if order_264_r == 0:
+    order_264_coupon = "yes"
+else:
+    order_264_coupon = "no"
+
+# Print out whether order #264 should receive a coupon
+print(order_264_coupon)
